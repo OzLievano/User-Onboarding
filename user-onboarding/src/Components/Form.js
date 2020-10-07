500e5da82d57a6e23b59fd891af70e6cc570e7a5
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-10-06 22:37:12 -0400

    recreated form

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -12,8 +12,38 @@ const AdvancedForm = () => {
     return (
         <form>
             <label htmlFor="name">
-                <input id="name" type="text">Name</input>
-            </label>
+                Name 
+                <input
+                id="name"
+                type="text"
+                name="name"
+                />
+            </label><br/>
+            <label htmlFor="email">
+                Email 
+                <input
+                id="email"
+                type="text"
+                name="email"
+                />
+            </label><br/>
+            <label htmlFor="pass">
+                Password
+                <input
+                id="pass"
+                type="password"
+                name="pass"
+                />
+            </label><br/>
+            <label htmlFor="terms">
+                Terms & Conditions
+                <input
+                id="terms"
+                type="checkbox"
+                name="terms"
+                />
+            </label><br/>
+            <button>SUBMIT</button>
         </form>
     )
 }
