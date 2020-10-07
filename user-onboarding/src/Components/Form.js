d4d4eeca7014504c19b13763b57efbc26179e741
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-10-06 21:35:41 -0400

    created form schema

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -9,23 +9,31 @@ const AdvancedForm = () => {
         password:"",
         terms:false,
     })
+
+    // add a schema for form validation 
+    const formSchema = yup.object().shape({
+        name:yup.string().required("Name is required."),
+        email:yup.string().email().required("email is required"),
+        password:yup.string().min(8).required("password required to be 8 characters"),
+        terms:yup.boolean().oneOf([true])
+    })
     return(
         <form>
-            <label htmlFor="name">Name</label>
+            <label htmlFor="name">Name </label>
             <input
                 type="text"
                 id="name"
                 name="name"
                 required
             /><br/>
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Email </label>
             <input
                 type="email"
                 id="email"
                 name="name"
                 required
             /><br/>
-            <label htmlFor="pass">Password (8 characters minimum)</label>
+            <label htmlFor="pass">Password </label>
             <input 
                 type="password"
                 id="pass"
