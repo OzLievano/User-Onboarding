289098475b9b8c0f0a981e615665e3351546f852
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-10-06 22:50:14 -0400

    creating validate function and set error state

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -10,12 +10,39 @@ const AdvancedForm = () => {
         terms:false
     })
 
+    const [errors,setErrors] = useState({
+        name:"",
+        email:"",
+        password:"",
+        terms:false
+    })
+    const validateChange = (e) =>{
+        yup.reach(formSchema,e.target.name)
+        .validate(
+            e.target.type==="checkbox"?e.target.checked : e.target.value
+        )
+        .then((valid)=>{
+            setErrors({...errors,[e.target.name]:""})
+        })
+        .catch((err)=>{
+            setErrors({...errors,[e.target.name]:err.errors[0]})
+        })
+    }
+
+    const formSchema = yup.object().shape({
+        name:yup.string().required("Name is required."),
+        email:yup.string().email(),
+        password:yup.string(),
+        terms:yup.boolean().oneOf([true])
+    });
+
     const inputChange = (e) =>{
         e.persist();
         const newForm = {
             ...formState,
             [e.target.name]:e.target.type==="checkbox" ? e.target.checked : e.target.value
         };
+        validateChange(e);
         setFormState(newForm);
     }
     return (
